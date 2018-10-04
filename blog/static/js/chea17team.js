2bc2d432dd05d9069d3928e443390b9dd2476800
abish97 <kumarabhishek22101997@gmail.com>
2018-10-05 00:45:48 +0530

    addingfiles

---
+++ blog/static/js/chea17team.js
@@ -0,0 +1,343 @@
+var change1 = function(){
+  $('.box1 .el').css("opacity", "0.0");
+
+
+  $('.box1').css("background", "url(https://raw.githubusercontent.com/abish97/chea/master/12.jpg)");
+  $('.box1 .number').css("opacity", "0.0");
+  $('.box1 .name').css("color", "white");
+  $('.box1 .position').css("color", "white");
+  $('i').css("opacity", "1.0");
+}
+var revert1 = function(){
+  $('.box1 .el').css("opacity", "1.0");
+  $('.box1').css("background", "none");
+  $('.box1 .number').css("opacity", "1.0");
+  $('.box1 .name').css("color", "black");
+  $('.box1 .position').css("color", "black");
+}
+
+var change2 = function(){
+  $('.box2 .el').css("opacity", "0.0");
+  $('.box2').css("background", "url(https://raw.githubusercontent.com/abish97/chea/master/12.jpg)");
+  $('.box2 .number').css("opacity", "0.0");
+
+  $('.box2 .name').css("color", "white");
+  $('.box2 .position').css("color", "white");
+  $('i').css("opacity", "1.0");
+}
+var revert2 = function(){
+  $('.box2 .el').css("opacity", "1.0");
+  $('.box2').css("background", "none");
+  $('.box2 .number').css("opacity", "1.0");
+
+  $('.box2 .name').css("color", "black");
+  $('.box2 .position').css("color", "black");
+}
+var change3 = function(){
+  $('.box3 .el').css("opacity", "0.0");
+  $('.box3').css("background", "url(https://raw.githubusercontent.com/abish97/chea/master/12.jpg)");
+  $('.box3 .number').css("opacity", "0.0");
+
+  $('.box3 .name').css("color", "white");
+  $('.box3 .position').css("color", "white");
+  $('i').css("opacity", "1.0");
+}
+var revert3 = function(){
+  $('.box3 .el').css("opacity", "1.0");
+  $('.box3').css("background", "none");
+  $('.box3 .number').css("opacity", "1.0");
+
+  $('.box3 .name').css("color", "black");
+  $('.box3 .position').css("color", "black");
+}
+var change4 = function(){
+  $('.box4 .el').css("opacity", "0.0");
+  $('.box4').css("background", "url(https://raw.githubusercontent.com/abish97/chea/master/12.jpg)");
+  $('.box4 .number').css("opacity", "0.0");
+  $('.box4').css("background-repeat","no-repeat")
+  $('.box4 .name').css("color", "white");
+  $('.box4 .position').css("color", "white");
+  $('i').css("opacity", "1.0");
+}
+var revert4 = function(){
+  $('.box4 .el').css("opacity", "1.0");
+  $('.box4').css("background", "none");
+  $('.box4 .number').css("opacity", "1.0");
+
+  $('.box4 .name').css("color", "black");
+  $('.box4 .position').css("color", "black");
+}
+var change5 = function(){
+  $('.box5 .el').css("opacity", "0.0");
+
+
+$('.box5').css("background", "url(https://raw.githubusercontent.com/abish97/chea/master/12.jpg)");
+  $('.box5 .number').css("opacity", "0.0");
+  $('.box5 .name').css("color", "white");
+  $('.box5 .position').css("color", "white");
+  $('i').css("opacity", "1.0");
+}
+var revert5 = function(){
+  $('.box5 .el').css("opacity", "1.0");
+
+  $('.box5').css("background", "none");
+  $('.box5 .number').css("opacity", "1.0");
+
+  $('.box5 .name').css("color", "black");
+  $('.box5 .position').css("color", "black");
+}
+var change6 = function(){
+  $('.box6 .el').css("opacity", "0.0");
+  $('.box6').css("background", "url(https://raw.githubusercontent.com/abish97/chea/master/12.jpg)");
+  $('.box6 .number').css("opacity", "0.0");
+
+  $('.box6 .name').css("color", "white");
+  $('.box6 .position').css("color", "white");
+  $('i').css("opacity", "1.0");
+}
+var revert6 = function(){
+  $('.box6 .el').css("opacity", "1.0");
+  $('.box6').css("background", "none");
+  $('.box6 .number').css("opacity", "1.0");
+
+  $('.box6 .name').css("color", "black");
+  $('.box6 .position').css("color", "black");
+}
+var change7 = function(){
+  $('.box7 .el').css("opacity", "0.0");
+  $('.box7').css("background", "url(https://raw.githubusercontent.com/abish97/chea/master/12.jpg)");
+  $('.box7 .number').css("opacity", "0.0");
+  $('.box7').css("background-repeat","no-repeat")
+  $('.box7 .name').css("color", "white");
+  $('.box7 .position').css("color", "white");
+  $('i').css("opacity", "1.0");
+}
+var revert7 = function(){
+  $('.box7 .el').css("opacity", "1.0");
+  $('.box7').css("background", "none");
+  $('.box7 .number').css("opacity", "1.0");
+
+  $('.box7 .name').css("color", "black");
+  $('.box7 .position').css("color", "black");
+}
+var change8 = function(){
+  $('.box8 .el').css("opacity", "0.0");
+  $('.box8').css("background", "url(https://raw.githubusercontent.com/abish97/chea/master/12.jpg)");
+  $('.box8 .number').css("opacity", "0.0");
+
+  $('.box8 .name').css("color", "white");
+  $('.box8 .position').css("color", "white");
+  $('i').css("opacity", "1.0");
+}
+var revert8 = function(){
+  $('.box8 .el').css("opacity", "1.0");
+  $('.box8').css("background", "none");
+  $('.box8 .number').css("opacity", "1.0");
+
+  $('.box8 .name').css("color", "black");
+  $('.box8 .position').css("color", "black");
+}
+var change9 = function(){
+  $('.box9 .el').css("opacity", "0.0");
+  $('.box9').css("background", "url(https://raw.githubusercontent.com/abish97/chea/master/12.jpg)");
+  $('.box9 .number').css("opacity", "0.0");
+
+  $('.box9 .name').css("color", "white");
+  $('.box9 .position').css("color", "white");
+  $('i').css("opacity", "1.0");
+}
+var revert9 = function(){
+  $('.box9 .el').css("opacity", "1.0");
+  $('.box9').css("background", "none");
+  $('.box9 .number').css("opacity", "1.0");
+
+  $('.box9 .name').css("color", "black");
+  $('.box9 .position').css("color", "black");
+}
+var change10 = function(){
+  $('.box10 .el').css("opacity", "0.0");
+  $('.box10').css("background", "url(https://raw.githubusercontent.com/abish97/chea/master/12.jpg)");
+  $('.box10 .number').css("opacity", "0.0");
+
+  $('.box10 .name').css("color", "white");
+  $('.box10 .position').css("color", "white");
+  $('i').css("opacity", "1.0");
+}
+var revert10 = function(){
+  $('.box10 .el').css("opacity", "1.0");
+  $('.box10').css("background", "none");
+  $('.box10 .number').css("opacity", "1.0");
+
+  $('.box10 .name').css("color", "black");
+  $('.box10 .position').css("color", "black");
+}
+var change11 = function(){
+  $('.box11 .el').css("opacity", "0.0");
+  $('.box11').css("background", "url(https://raw.githubusercontent.com/abish97/chea/master/12.jpg)");
+  $('.box11 .number').css("opacity", "0.0");
+
+  $('.box11 .name').css("color", "white");
+  $('.box11 .position').css("color", "white");
+  $('i').css("opacity", "1.0");
+}
+var revert11 = function(){
+  $('.box11 .el').css("opacity", "1.0");
+  $('.box11').css("background", "none");
+  $('.box11 .number').css("opacity", "1.0");
+
+  $('.box11 .name').css("color", "black");
+  $('.box11 .position').css("color", "black");
+}
+var change12 = function(){
+  $('.box12 .el').css("opacity", "0.0");
+  $('.box12').css("background", "url(https://raw.githubusercontent.com/abish97/chea/master/12.jpg)");
+  $('.box12 .number').css("opacity", "0.0");
+
+  $('.box12 .name').css("color", "white");
+  $('.box12 .position').css("color", "white");
+  $('i').css("opacity", "1.0");
+}
+var revert12 = function(){
+  $('.box12 .el').css("opacity", "1.0");
+  $('.box12').css("background", "none");
+  $('.box12 .number').css("opacity", "1.0");
+
+  $('.box12 .name').css("color", "black");
+  $('.box12 .position').css("color", "black");
+}
+var change13 = function(){
+  $('.box13 .el').css("opacity", "0.0");
+  $('.box13').css("background", "url(https://raw.githubusercontent.com/abish97/chea/master/12.jpg)");
+  $('.box13 .number').css("opacity", "0.0");
+
+  $('.box13 .name').css("color", "white");
+  $('.box13 .position').css("color", "white");
+  $('i').css("opacity", "1.0");
+}
+var revert13 = function(){
+  $('.box13 .el').css("opacity", "1.0");
+  $('.box13').css("background", "none");
+  $('.box13 .number').css("opacity", "1.0");
+
+  $('.box13 .name').css("color", "black");
+  $('.box13 .position').css("color", "black");
+}
+var change14 = function(){
+  $('.box14 .el').css("opacity", "0.0");
+  $('.box14').css("background", "url(https://raw.githubusercontent.com/abish97/chea/master/12.jpg)");
+  $('.box14 .number').css("opacity", "0.0");
+
+  $('.box14 .name').css("color", "white");
+  $('.box14 .position').css("color", "white");
+  $('i').css("opacity", "1.0");
+}
+var revert14 = function(){
+  $('.box14 .el').css("opacity", "1.0");
+  $('.box14').css("background", "none");
+  $('.box14 .number').css("opacity", "1.0");
+
+  $('.box14 .name').css("color", "black");
+  $('.box14 .position').css("color", "black");
+}
+var change15 = function(){
+  $('.box15 .el').css("opacity", "0.0");
+  $('.box15').css("background", "url(https://raw.githubusercontent.com/abish97/chea/master/12.jpg");
+  $('.box15 .number').css("opacity", "0.0");
+  $('.box15').css("background-repeat","no-repeat")
+  $('.box15 .name').css("color", "white");
+  $('.box15 .position').css("color", "white");
+  $('i').css("opacity", "1.0");
+}
+var revert15 = function(){
+  $('.box15 .el').css("opacity", "1.0");
+  $('.box15').css("background", "none");
+  $('.box15 .number').css("opacity", "1.0");
+
+  $('.box15 .name').css("color", "black");
+  $('.box15 .position').css("color", "black");
+}
+var change16 = function(){
+  $('.box16 .el').css("opacity", "0.0");
+  $('.box16').css("background", "url(https://raw.githubusercontent.com/abish97/chea/master/12.jpg)");
+  $('.box16 .number').css("opacity", "0.0");
+
+  $('.box16 .name').css("color", "white");
+  $('.box16 .position').css("color", "white");
+  $('i').css("opacity", "1.0");
+}
+var revert16 = function(){
+  $('.box16 .el').css("opacity", "1.0");
+  $('.box16').css("background", "none");
+  $('.box16 .number').css("opacity", "1.0");
+
+  $('.box16 .name').css("color", "black");
+  $('.box16 .position').css("color", "black");
+}
+var change17 = function(){
+  $('.box17 .el').css("opacity", "0.0");
+  $('.box17').css("background", "url(https://raw.githubusercontent.com/abish97/chea/master/12.jpg)");
+  $('.box17 .number').css("opacity", "0.0");
+
+  $('.box17 .name').css("color", "white");
+  $('.box17 .position').css("color", "white");
+  $('i').css("opacity", "1.0");
+}
+var revert17 = function(){
+  $('.box17 .el').css("opacity", "1.0");
+  $('.box17').css("background", "none");
+  $('.box17 .number').css("opacity", "1.0");
+
+  $('.box17 .name').css("color", "black");
+  $('.box17 .position').css("color", "black");
+}
+var change18 = function(){
+  $('.box18 .el').css("opacity", "0.0");
+  $('.box18').css("background", "url(https://raw.githubusercontent.com/abish97/chea/master/12.jpg)");
+  $('.box18 .number').css("opacity", "0.0");
+
+  $('.box18 .name').css("color", "white");
+  $('.box18 .position').css("color", "white");
+  $('i').css("opacity", "1.0");
+}
+var revert18 = function(){
+  $('.box18 .el').css("opacity", "1.0");
+  $('.box18').css("background", "none");
+  $('.box18 .number').css("opacity", "1.0");
+
+  $('.box18 .name').css("color", "black");
+  $('.box18 .position').css("color", "black");
+}
+var change19 = function(){
+  $('.box19 .el').css("opacity", "0.0");
+  $('.box19').css("background", "url(https://raw.githubusercontent.com/abish97/chea/master/12.jpg)");
+  $('.box19 .number').css("opacity", "0.0");
+
+  $('.box19 .name').css("color", "white");
+  $('.box19 .position').css("color", "white");
+  $('i').css("opacity", "1.0");
+}
+var revert19 = function(){
+  $('.box19 .el').css("opacity", "1.0");
+  $('.box19').css("background", "none");
+  $('.box19 .number').css("opacity", "1.0");
+
+  $('.box19 .name').css("color", "black");
+  $('.box19 .position').css("color", "black");
+}
+var change20 = function(){
+  $('.box20 .el').css("opacity", "0.0");
+  $('.box20').css("background", "url(https://raw.githubusercontent.com/abish97/chea/master/12.jpg)");
+  $('.box20 .number').css("opacity", "0.0");
+
+  $('.box20 .name').css("color", "white");
+  $('.box20 .position').css("color", "white");
+  $('i').css("opacity", "1.0");
+}
+var revert20 = function(){
+  $('.box20 .el').css("opacity", "1.0");
+  $('.box20').css("background", "none");
+  $('.box20 .number').css("opacity", "1.0");
+
+  $('.box20 .name').css("color", "black");
+  $('.box20 .position').css("color", "black");
+}
---
+++ blog/static/js/iiche17team.js
@@ -0,0 +1,105 @@
+var change1 = function(){
+  $('.box1 .el').css("opacity", "0.0");
+
+
+  $('.box1').css("background", "url(https://raw.githubusercontent.com/abish97/chea/master/12.jpg)");
+  $('.box1 .number').css("opacity", "0.0");
+  $('.box1 .name').css("color", "white");
+  $('.box1 .position').css("color", "white");
+  $('i').css("opacity", "1.0");
+}
+var revert1 = function(){
+  $('.box1 .el').css("opacity", "1.0");
+  $('.box1').css("background", "none");
+  $('.box1 .number').css("opacity", "1.0");
+  $('.box1 .name').css("color", "black");
+  $('.box1 .position').css("color", "black");
+}
+
+var change2 = function(){
+  $('.box2 .el').css("opacity", "0.0");
+  $('.box2').css("background", "url(https://raw.githubusercontent.com/abish97/chea/master/12.jpg)");
+  $('.box2 .number').css("opacity", "0.0");
+
+  $('.box2 .name').css("color", "white");
+  $('.box2 .position').css("color", "white");
+  $('i').css("opacity", "1.0");
+}
+var revert2 = function(){
+  $('.box2 .el').css("opacity", "1.0");
+  $('.box2').css("background", "none");
+  $('.box2 .number').css("opacity", "1.0");
+
+  $('.box2 .name').css("color", "black");
+  $('.box2 .position').css("color", "black");
+}
+var change3 = function(){
+  $('.box3 .el').css("opacity", "0.0");
+  $('.box3').css("background", "url(https://raw.githubusercontent.com/abish97/chea/master/12.jpg)");
+  $('.box3 .number').css("opacity", "0.0");
+
+  $('.box3 .name').css("color", "white");
+  $('.box3 .position').css("color", "white");
+  $('i').css("opacity", "1.0");
+}
+var revert3 = function(){
+  $('.box3 .el').css("opacity", "1.0");
+  $('.box3').css("background", "none");
+  $('.box3 .number').css("opacity", "1.0");
+
+  $('.box3 .name').css("color", "black");
+  $('.box3 .position').css("color", "black");
+}
+var change4 = function(){
+  $('.box4 .el').css("opacity", "0.0");
+  $('.box4').css("background", "url(https://raw.githubusercontent.com/abish97/chea/master/12.jpg)");
+  $('.box4 .number').css("opacity", "0.0");
+  $('.box4').css("background-repeat","no-repeat")
+  $('.box4 .name').css("color", "white");
+  $('.box4 .position').css("color", "white");
+  $('i').css("opacity", "1.0");
+}
+var revert4 = function(){
+  $('.box4 .el').css("opacity", "1.0");
+  $('.box4').css("background", "none");
+  $('.box4 .number').css("opacity", "1.0");
+
+  $('.box4 .name').css("color", "black");
+  $('.box4 .position').css("color", "black");
+}
+var change5 = function(){
+  $('.box5 .el').css("opacity", "0.0");
+
+
+$('.box5').css("background", "url(https://raw.githubusercontent.com/abish97/chea/master/12.jpg)");
+  $('.box5 .number').css("opacity", "0.0");
+  $('.box5 .name').css("color", "white");
+  $('.box5 .position').css("color", "white");
+  $('i').css("opacity", "1.0");
+}
+var revert5 = function(){
+  $('.box5 .el').css("opacity", "1.0");
+
+  $('.box5').css("background", "none");
+  $('.box5 .number').css("opacity", "1.0");
+
+  $('.box5 .name').css("color", "black");
+  $('.box5 .position').css("color", "black");
+}
+var change6 = function(){
+  $('.box6 .el').css("opacity", "0.0");
+  $('.box6').css("background", "url(https://raw.githubusercontent.com/abish97/chea/master/12.jpg)");
+  $('.box6 .number').css("opacity", "0.0");
+
+  $('.box6 .name').css("color", "white");
+  $('.box6 .position').css("color", "white");
+  $('i').css("opacity", "1.0");
+}
+var revert6 = function(){
+  $('.box6 .el').css("opacity", "1.0");
+  $('.box6').css("background", "none");
+  $('.box6 .number').css("opacity", "1.0");
+
+  $('.box6 .name').css("color", "black");
+  $('.box6 .position').css("color", "black");
+}
